e8bbb93b98f2a0957324a555665279088a50a18d
wizard1066 <mark.lucking@gmail.com>
2022-08-09 10:21:10 +0200

    Initial Commit

---
+++ Ball/BallApp.swift
@@ -0,0 +1,17 @@
+//
+//  BallApp.swift
+//  Ball
+//
+//  Created by localuser on 09.08.22.
+//
+
+import SwiftUI
+
+@main
+struct BallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
